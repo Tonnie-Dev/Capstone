e8f31192b98b9befb31da6e8e650ddc9a32ae292
Antony <vontonnie@gmail.com>
2021-04-18 19:01:06 +0300

    Declare ElectionsViewModel using Kotlin Property Delegate

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
 
 class ElectionsFragment: Fragment() {
 
     //TODO: Declare ViewModel
-
+    val viewModel:ElectionsViewModel by viewModels()
+    private lateinit var  binding: FragmentLaunchBinding
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
 
         //TODO: Add ViewModel values and create ViewModel
 
@@ -23,6 +26,9 @@ class ElectionsFragment: Fragment() {
         //TODO: Initiate recycler adapters
 
         //TODO: Populate recycler adapters
+
+        binding = FragmentLaunchBinding.inflate(inflater)
+        return binding.root
 
     }
 
