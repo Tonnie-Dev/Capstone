8047fb9d65de5bad1528aa6bbf95949722727a93
Antony <vontonnie@gmail.com>
2021-04-23 12:46:37 +0300

    Add ElectionViewHolder class

--- app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ElectionItemBinding
 import com.example.android.politicalpreparedness.databinding.ViewholderElectionBinding
 import com.example.android.politicalpreparedness.network.models.Election
 
-class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
+class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionListAdapter.ElectionViewHolder>(ElectionDiffCallback()) {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
@@ -17,8 +20,11 @@ class ElectionListAdapter(private val clickListener: ElectionListener): ListAdap
     //TODO: Bind ViewHolder
     override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
 
+        val binding = ElectionItemBinding.inflate(inflater, parent, false)
     }
+    class ElectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    }
     class ElectionDiffCallback: DiffUtil.ItemCallback<Election>() {
         override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
             return oldItem.id == newItem.id
@@ -29,6 +35,8 @@ class ElectionListAdapter(private val clickListener: ElectionListener): ListAdap
         }
 
     }
+
+
 
     //TODO: Add companion object to inflate ViewHolder (from)
 }
