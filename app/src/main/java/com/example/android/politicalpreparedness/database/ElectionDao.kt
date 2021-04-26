79a041012d51afe615172d8ccf78fd54da33504b
Antony <vontonnie@gmail.com>
2021-04-26 06:49:42 +0300

    Add getElectionById query

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -10,7 +10,7 @@ interface ElectionDao {
     /*insert a list of elections fetched from the network into the database. If and
     election entry is present in database then it is overwritten using REPLACE Strategy*/
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertElections(elections:List<Election>)
+    fun insertElections(elections: List<Election>)
 
 
     //TODO: Add select all election query
@@ -19,7 +19,8 @@ interface ElectionDao {
     fun getAllSavedElections(): LiveData<List<Election>>
 
     //TODO: Add select single election query
-
+    @Query("SELECT * FROM election_table WHERE id=:key")
+    fun getElectionById(key: Int)
     //TODO: Add delete query
 
     //TODO: Add clear query
