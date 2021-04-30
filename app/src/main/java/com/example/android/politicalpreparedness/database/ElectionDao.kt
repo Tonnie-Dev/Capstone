5461b29593a469dd27f69c18b062ccbd68f72142
Antony <vontonnie@gmail.com>
2021-04-30 08:23:07 +0300

    Add insertFollowedElection

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -8,29 +8,26 @@ import com.example.android.politicalpreparedness.network.models.Election
 interface ElectionDao {
 
 
-    @Query("SELECT * FROM election_table WHERE id =:key")
-    fun getElectionById(key: Int): Election
-
-
     /*insert a list of elections fetched from the network into the database. If and
     election entry is present in database then it is overwritten using REPLACE Strategy*/
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertElections(elections: List<Election>)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFollowedElection( followedElectionId: Int)
+
+    @Query("SELECT * FROM election_table WHERE id =:key")
+    fun getElectionById(key: Int): Election
 
     @Query("SELECT * FROM election_table ORDER BY electionDay ASC")
     fun getAllSavedElections(): LiveData<List<Election>>
 
     @Query("SELECT * FROM election_table WHERE id in (SELECT * FROM followed_elections_table) ORDER BY electionDay DESC")
     fun getFollowedElections(): LiveData<List<Election>>
-   
 
-    //TODO: Add delete query
     @Query("DELETE FROM election_table WHERE id = :id")
     fun deleteElections(id: Int)
 
-
-    //TODO: Add clear query
     @Query("DELETE FROM election_table")
     fun clearElections()
 
