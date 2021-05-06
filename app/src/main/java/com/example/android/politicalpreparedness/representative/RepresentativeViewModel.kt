7d25a2c285b6a32f335867db0952df75d32a93d9
Antony <vontonnie@gmail.com>
2021-05-06 20:07:20 +0300

    Create Binding Adapter for Loading Status

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -1,85 +1,75 @@
 package com.example.android.politicalpreparedness.representative
 
 import android.app.Application
-import android.location.Geocoder
-import android.location.Location
 import androidx.lifecycle.*
 import com.example.android.politicalpreparedness.network.CivicsApi
 import com.example.android.politicalpreparedness.network.models.Address
 import com.example.android.politicalpreparedness.representative.model.Representative
+import java.util.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import timber.log.Timber
-import java.util.*
 
 class RepresentativeViewModel(application: Application) : AndroidViewModel(application) {
 
-    //TODO: Establish live data for representatives and address
+  // TODO: Establish live data for representatives and address
 
-    private val _reps = MutableLiveData<List<Representative>>()
-    val reps: LiveData<List<Representative>>
-        get() = _reps
+  private val _reps = MutableLiveData<List<Representative>>()
+  val reps: LiveData<List<Representative>>
+    get() = _reps
 
-    private val _address = MutableLiveData<Address>()
-    val address: LiveData<Address>
-        get() = _address
+  private val _address = MutableLiveData<Address>()
+  val address: LiveData<Address>
+    get() = _address
 
-
-    private val _status = MutableLiveData<LoadingStatus>()
-    val status: LiveData<LoadingStatus>
+  private val _status = MutableLiveData<LoadingStatus>()
+  val status: LiveData<LoadingStatus>
     get() = _status
 
+  // TODO: Create function to fetch representatives from API from a provided address
 
-    //TODO: Create function to fetch representatives from API from a provided address
+  fun fetchRepsFromNetwork(address: String) {
 
-    fun fetchRepsFromNetwork(address:String) {
+    viewModelScope.launch {
+      try {
 
-        viewModelScope.launch {
-
-try {
-
-    _status.value = LoadingStatus.LOADING
-    withContext(IO) {
-
-        val (offices, officials) =CivicsApi.retrofitService.representativeInfoByAddress(address)
-        _reps.postValue (offices.flatMap { office ->office.getRepresentatives(officials)})
-_status.postValue(LoadingStatus.FINISHED)
-    }
-}catch (e:Exception) {
-
-    _status.value = LoadingStatus.ERROR
-}
-
-
-
+        _status.value = LoadingStatus.LOADING
+        withContext(IO) {
+          val (offices, officials) = CivicsApi.retrofitService.representativeInfoByAddress(address)
+          _reps.postValue(offices.flatMap { office -> office.getRepresentatives(officials) })
+          _status.postValue(LoadingStatus.FINISHED)
         }
+      } catch (e: Exception) {
 
-
+        _status.value = LoadingStatus.ERROR
+      }
     }
+  }
 
+  /**
+   * The following code will prove helpful in constructing a representative from the API. This code
+   * combines the two nodes of the RepresentativeResponse into a single official :
+   *
+   * val (offices, officials) = getRepresentativesDeferred.await() _representatives.value =
+   * offices.flatMap { office -> office.getRepresentatives(officials) }
+   *
+   * Note: getRepresentatives in the above code represents the method used to fetch data from the
+   * API Note: _representatives in the above code represents the established mutable live data
+   * housing representatives
+   */
 
-    /**
-     *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
+  // TODO: Create function get address from geo location
+  fun getAddressFromGeoLocation(address: Address) {
 
-    val (offices, officials) = getRepresentativesDeferred.await()
-    _representatives.value = offices.flatMap { office -> office.getRepresentatives(officials) }
+    _address.value = address
+  }
 
-    Note: getRepresentatives in the above code represents the method used to fetch data from the API
-    Note: _representatives in the above code represents the established mutable live data housing representatives
-
-     */
-
-    //TODO: Create function get address from geo location
-    fun getAddressFromGeoLocation(address:Address){
-
-        _address.value = address
-    }
-
-    //TODO: Create function to get address from individual fields
+  // TODO: Create function to get address from individual fields
 
 }
 
 enum class LoadingStatus {
-    LOADING, ERROR, FINISHED
+  LOADING,
+  ERROR,
+  FINISHED
 }
