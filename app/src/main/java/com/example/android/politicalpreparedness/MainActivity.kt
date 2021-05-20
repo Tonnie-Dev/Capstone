18bf940305d1477fe45f77531b4f55e5d2314d5f
Antony <vontonnie@gmail.com>
2021-05-20 18:45:10 +0300

    Update Kotlin Version

--- app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -8,7 +8,7 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
